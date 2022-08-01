9dd41f485ec0cb34a13031e62488fa468261bd79
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2022-08-02 02:57:37 +0300

    init
    init

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      textColor: {
+        skin: {
+          primary: withOpacity("--color-text-primary"),
+          secondary: withOpacity("--color-text-secondary"),
+          red: withOpacity("--color-red"),
+          yellow: withOpacity("--color-yellow"),
+          blue: withOpacity("--color-blue"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          primary: withOpacity("--color-background-primary"),
+          secondary: withOpacity("--color-background-secondary"),
+          tertiary: withOpacity("--color-background-tertiary"),
+          red: withOpacity("--color-red"),
+          yellow: withOpacity("--color-yellow"),
+          blue: withOpacity("--color-blue"),
+        },
+      },
+      borderColor: {
+        skin: {
+          primary: withOpacity("--color-text-primary"),
+          secondary: withOpacity("--color-text-secondary"),
+          red: withOpacity("--color-red"),
+          yellow: withOpacity("--color-yellow"),
+          blue: withOpacity("--color-blue"),
+        },
+      },
+    },
+  },
+  plugins: [],
+};
